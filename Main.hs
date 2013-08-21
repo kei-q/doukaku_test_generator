f84f8b6960569a6237bec5d2ce7e3726d915c607
keqh <keqh.keqh@gmail.com>
2013-08-22 00:06:00 +0900

    baseのversion次第でcatchが無く、errorになるので修正

--- Main.hs
+++ Main.hs
@@ -3,7 +3,7 @@
 import Web.Scotty
 import Network.HTTP.Types (notFound404)
 import Control.Monad.IO.Class (liftIO)
-import Control.Exception (catch, SomeException)
+import qualified Control.Exception as E (catch, SomeException)
 
 import Parser
 
@@ -22,7 +22,7 @@ main = scotty 3001 $ do
                     r <- parseTemplate ("template/" ++ format ++ ".tpl") td
                     return $ Just r
                 _ -> return Nothing
-            `catch` \(_::SomeException) -> return Nothing
+            `E.catch` \(_::E.SomeException) -> return Nothing
         case ret of
             Nothing -> r404
             Just r -> text r
